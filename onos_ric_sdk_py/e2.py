313df00de20d09c5bdb1ec4281671bb214806aab
Omar Abdelkader <omikader@fb.com>
2021-05-10 15:04:05 -0400

    Update e2 client with latest changes

--- onos_ric_sdk_py/e2.py
+++ onos_ric_sdk_py/e2.py
@@ -1,17 +1,20 @@
-# SPDX-FileCopyrightText: © 2021 Open Networking Foundation <support@opennetworking.org>
-# SPDX-License-Identifier: Apache-2.0
+#!/usr/bin/env python3
+# Copyright 2004-present Facebook. All Rights Reserved.
 
 import asyncio
 import ssl
 from typing import AsyncIterator, List, Optional, Tuple
 from uuid import uuid4
 
-import aiomsa.e2
 import betterproto
 from betterproto.grpc.util.async_channel import AsyncChannel
 from grpclib.client import Channel
-from onos_api.e2sub.endpoint import E2RegistryServiceStub
-from onos_api.e2sub.subscription import (
+
+from aiomsa import e2, models
+from aiomsa.exceptions import E2ClientError
+
+from .onos_api.onos.e2sub.endpoint import E2RegistryServiceStub
+from .onos_api.onos.e2sub.subscription import (
     Action,
     ActionType,
     E2SubscriptionServiceStub,
@@ -25,9 +28,9 @@ from onos_api.e2sub.subscription import (
     SubsequentActionType,
     TimeToWait,
 )
-from onos_api.e2sub.task import E2SubscriptionTaskServiceStub, EventType, Status
-from onos_api.e2t.admin import E2TAdminServiceStub
-from onos_api.e2t.e2 import (  # ResponseStatus,
+from .onos_api.onos.e2sub.task import E2SubscriptionTaskServiceStub, EventType, Status
+from .onos_api.onos.e2t.admin import E2TAdminServiceStub
+from .onos_api.onos.e2t.e2 import (  # ResponseStatus,
     ControlAckRequest,
     E2TServiceStub,
     EncodingType,
@@ -38,7 +41,7 @@ from onos_api.e2t.e2 import (  # ResponseStatus,
 )
 
 
-class Subscription(aiomsa.e2.Subscription):
+class Subscription(e2.Subscription):
     def __init__(
         self,
         subscription_id: str,
@@ -46,7 +49,7 @@ class Subscription(aiomsa.e2.Subscription):
         e2sub_channel: Channel,
         ssl_context: Optional[ssl.SSLContext],
     ) -> None:
-        self.subscription_id = subscription_id
+        self._id = subscription_id
         self._app_id = app_id
         self._watch_task = asyncio.create_task(self._watch(e2sub_channel, ssl_context))
         self._stream_lock = asyncio.Semaphore(0)
@@ -72,10 +75,10 @@ class Subscription(aiomsa.e2.Subscription):
         task_client = E2SubscriptionTaskServiceStub(e2sub_channel)
         prev_endpoint_id = None
         async for response in task_client.watch_subscription_tasks(
-            subscription_id=self.subscription_id
+            subscription_id=self._id
         ):
             event = response.event
-            if event.task.subscription_id != self.subscription_id:
+            if event.task.subscription_id != self._id:
                 # Only interested in tasks related to this subscription
                 continue
             if event.task.endpoint_id == prev_endpoint_id:
@@ -97,7 +100,7 @@ class Subscription(aiomsa.e2.Subscription):
                         [
                             StreamRequest(
                                 app_id=self._app_id,
-                                subscription_id=self.subscription_id,
+                                subscription_id=self._id,
                             )
                         ],
                         close=True,
@@ -109,6 +112,10 @@ class Subscription(aiomsa.e2.Subscription):
                 await self._stream_lock.acquire()
                 self._stream = None
 
+    @property
+    def id(self) -> str:
+        return self._id
+
     def __aiter__(self) -> "Subscription":
         return self
 
@@ -117,7 +124,7 @@ class Subscription(aiomsa.e2.Subscription):
             return await self.getone()
 
 
-class E2Client(aiomsa.e2.E2Client):
+class E2Client(e2.E2Client):
     def __init__(
         self,
         app_id: str,
@@ -143,7 +150,7 @@ class E2Client(aiomsa.e2.E2Client):
         self._e2t_channel = Channel(e2t_ip, int(e2t_port), ssl=self._ssl_context)
         self._e2sub_channel = Channel(e2sub_ip, int(e2sub_port), ssl=self._ssl_context)
 
-    async def list_nodes(self, oid: Optional[str] = None) -> List[aiomsa.e2.E2Node]:
+    async def list_nodes(self, oid: Optional[str] = None) -> List[models.E2Node]:
         nodes = []
 
         admin_client = E2TAdminServiceStub(self._e2t_channel)
@@ -152,10 +159,10 @@ class E2Client(aiomsa.e2.E2Client):
                 continue
             if oid is None or any(func.oid == oid for func in conn.ran_functions):
                 nodes.append(
-                    aiomsa.e2.E2Node(
+                    models.E2Node(
                         id=conn.id,
                         ran_functions=[
-                            aiomsa.e2.RanFunction(
+                            models.RanFunction(
                                 id=func.ran_function_id,
                                 oid=func.oid,
                                 definition=func.description,
@@ -174,7 +181,7 @@ class E2Client(aiomsa.e2.E2Client):
         service_model_version: str,
         header: bytes,
         message: bytes,
-        control_ack_request: aiomsa.e2.ControlAckRequest,
+        control_ack_request: models.RICControlAckRequest,
     ) -> Optional[bytes]:
         e2t_client = E2TServiceStub(self._e2t_channel)
         request_header = RequestHeader(
@@ -193,9 +200,9 @@ class E2Client(aiomsa.e2.E2Client):
         )
         name, value = betterproto.which_one_of(control_response, "response")
         if value is None:
-            raise Exception("E2 control response is missing")
+            raise E2ClientError("E2 control response is missing")
         if name == "control_failure":
-            raise Exception(f"Control failure ({value.cause}): {value.message}")
+            raise E2ClientError(f"Control failure ({value.cause}): {value.message}")
 
         # The value of response is `control_acknowledge`
         if control_ack_request == ControlAckRequest.NO_ACK:
@@ -214,7 +221,7 @@ class E2Client(aiomsa.e2.E2Client):
         service_model_name: str,
         service_model_version: str,
         trigger: bytes,
-        actions: List[aiomsa.e2.Action],
+        actions: List[models.RICAction],
     ) -> Subscription:
         subscription_client = E2SubscriptionServiceStub(self._e2sub_channel)
         subscription = E2Subscription(
@@ -228,26 +235,21 @@ class E2Client(aiomsa.e2.E2Client):
                 event_trigger=EventTrigger(
                     payload=Payload(encoding=Encoding.ENCODING_PROTO, data=trigger)
                 ),
-                actions=[
-                    Action(
-                        id=action.id,
-                        type=ActionType(action.type),
-                        payload=(
-                            Payload()
-                            if action.data is None
-                            else Payload(
-                                encoding=Encoding.ENCODING_PROTO, data=action.data
-                            )
-                        ),
-                        subsequent_action=SubsequentAction(
-                            type=SubsequentActionType(action.subsequent_action_type),
-                            time_to_wait=TimeToWait(action.time_to_wait),
-                        ),
-                    )
-                    for action in actions
-                ],
             ),
         )
+        for a in actions:
+            action = Action(id=a.id, type=ActionType(a.type))
+            if a.definition is not None:
+                action.payload = Payload(
+                    encoding=Encoding.ENCODING_PROTO, data=a.definition
+                )
+            if a.subsequent_action is not None:
+                action.subsequent_action = SubsequentAction(
+                    type=SubsequentActionType(a.subsequent_action.type),
+                    time_to_wait=TimeToWait(a.subsequent_action.time_to_wait),
+                )
+            subscription.details.actions.append(action)
+
         await subscription_client.add_subscription(subscription=subscription)
         return Subscription(
             subscription.id,
@@ -266,3 +268,4 @@ class E2Client(aiomsa.e2.E2Client):
     async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         self._e2t_channel.close()
         self._e2sub_channel.close()
+
